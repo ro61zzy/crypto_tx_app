6ba6ca273c9936541be31262ce62942eb6fa19ff
rozzy <rosekaremeri@gmail.com>
2025-01-02 16:25:33 +0300

    Feature: add sepolia

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,6 +1,28 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+  require("@nomicfoundation/hardhat-toolbox");
+
+  const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+  const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+  if (!PRIVATE_KEY) {
+    console.error("Please set your PRIVATE_KEY in a .env file");
+    process.exit(1);
+  }
+
+  if (!ALCHEMY_API_KEY) {
+    console.error("Please set your ALCHEMY_API_KEY in a .env file");
+    process.exit(1);
+  }
+
+  console.log("Sepolia URL:", `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`);
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
